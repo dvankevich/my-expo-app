4a2478fb0b3f6718cabc3ee543a8bf4ea1b903c7
dvankevich <dvankevich@gmail.com>
2025-01-29 22:45:43 +0200

    Navigate between pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Link } from "expo-router";
 
-export default function Index() {
+export default function Page() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Link href="/about">About</Link>
+      {/* ...other links */}
+      <Link href="/user/bacon">View user</Link>
     </View>
   );
 }
