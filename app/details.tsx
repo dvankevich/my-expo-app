e688721cee5ae0fbc22e92f3bd4dcdd3ff5725ee
dvankevich <dvankevich@gmail.com>
2025-01-30 19:18:53 +0200

    use Optional Chaining

--- app/details.tsx
+++ app/details.tsx
@@ -7,13 +7,19 @@ export default function Details() {
 
   // Приведення params.name до типу string
   // const title = Array.isArray(params.name) ? params.name[0] : params.name;
+  // or
   // Використання оператора нульового злиття для безпечного отримання рядка
+  // const title =
+  //   typeof params.name === "string"
+  //     ? params.name
+  //     : Array.isArray(params.name)
+  //     ? params.name[0]
+  //     : "Default Title";
+  // Використання Optional Chaining та нульового злиття
   const title =
     typeof params.name === "string"
       ? params.name
-      : Array.isArray(params.name)
-      ? params.name[0]
-      : "Default Title";
+      : params.name?.[0] ?? "Default Title";
 
   return (
     <View style={styles.container}>
