aaec0489f647c1c5d2355c2d8e04478515b9e0a7
dvankevich <dvankevich@gmail.com>
2025-01-31 20:14:41 +0200

    Nesting navigators

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,3 @@
 import { Stack } from "expo-router";
 
-export default function Layout() {
-  return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#f4511e",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen
-        name="user/[id]"
-        getId={({ params }) => String(Date.now())}
-      />
-    </Stack>
-  );
-}
+export default Stack;
--- app/index.tsx
+++ app/index.tsx
@@ -1,87 +1,18 @@
-import { Link, Stack } from "expo-router";
-import {
-  Button,
-  Text,
-  Image,
-  StyleSheet,
-  View,
-  TouchableOpacity,
-} from "react-native";
-import { useState } from "react";
+import { Link } from "expo-router";
+import { View } from "react-native";
 
-function LogoTitle() {
+export default function Root() {
   return (
-    <Image
-      style={styles.image}
-      source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
-    />
-  );
-}
-
-export default function Home() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <View style={styles.container}>
-      <Stack.Screen
-        options={{
-          headerTitle: (props) => <LogoTitle />,
-          headerRight: () => (
-            <Button
-              onPress={() => setCount((c) => c + 1)}
-              title="Update count"
-            />
-          ),
-        }}
-      />
-      <Text>Count: {count}</Text>
-      <TouchableOpacity style={styles.buttonContainer}>
-        <Link href={{ pathname: "/details", params: { name: "Bacon" } }}>
-          <Text style={styles.buttonText}>Go to Details</Text>
-        </Link>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.buttonContainer}>
-        <Link href={{ pathname: "/settings", params: { name: "Settings" } }}>
-          <Text style={styles.buttonText}>Go to Settings</Text>
-        </Link>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.buttonContainer}>
-        <Link href={{ pathname: "/user/[id]", params: { id: "123" } }}>
-          <Text style={styles.buttonText}>Go to User</Text>
-        </Link>
-      </TouchableOpacity>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 25,
+      }}
+    >
+      <Link href="/home/messages">Navigate to nested route /home/messages</Link>
+      <Link href="/home/feed">Navigate to nested route /home/feed</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  image: {
-    width: 50,
-    height: 50,
-  },
-  button: {
-    padding: 10,
-    backgroundColor: "#007BFF",
-    color: "#FFFFFF",
-    borderRadius: 5,
-    textAlign: "center",
-    marginVertical: 10,
-  },
-  buttonContainer: {
-    backgroundColor: "#FF5733", // Змініть колір на бажаний
-    borderRadius: 10,
-    paddingVertical: 15, // Збільшення відступу по вертикалі
-    paddingHorizontal: 30, // Збільшення відступу по горизонталі
-    marginVertical: 10,
-  },
-  buttonText: {
-    color: "#FFFFFF",
-    fontSize: 18, // Збільшення розміру шрифту
-    textAlign: "center",
-  },
-});
