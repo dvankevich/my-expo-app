941ab20dceddeae0b526d314a4b346eec7cb5aa9
dvankevich <dvankevich@gmail.com>
2025-01-31 19:25:31 +0200

    use the Drawer layout in Expo Router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,25 @@
-import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
 
 export default function Layout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#f4511e",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen
-        name="user/[id]"
-        getId={({ params }) => String(Date.now())}
-      />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer>
+        <Drawer.Screen
+          name="index" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: "Home",
+            title: "overview",
+          }}
+        />
+        <Drawer.Screen
+          name="user/[id]" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: "User",
+            title: "overview",
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
   );
 }
